f8618d2cb35d5e1d02c8b192317602c71849e4ad
Njoki Kamau <njokifaith61@gmail.com>
2020-07-15 21:32:31 +0300

    add basic routes in app.java to different pages in app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,38 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
         get("/hello", (request, response) -> "Hello Friend!");
+        staticFileLocation("/public");
+
+        //Display homepage
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "homepage.hbs");
+        }, new HandlebarsTemplateEngine());
+        //display hero profiles
+        get("/guide", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "guide.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/sighting-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "sighting-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        //display hero profiles
+        get("/sightings", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "all-sightings.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/endangered", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "endangered-species.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
