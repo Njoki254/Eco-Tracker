54db887694bca95cdd9277610343592e94afb664
Njoki Kamau <njokifaith61@gmail.com>
2020-07-16 07:31:08 +0300

    add sighting form code in templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        get("/hello", (request, response) -> "Hello Friend!");
+
         staticFileLocation("/public");
 
         //Display homepage
